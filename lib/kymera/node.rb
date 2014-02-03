96b2c6cd7ce014b8416e878092eeffcdb7818087
jakesa <jakes55214@yahoo.com>
2014-02-03 14:10:23 -0800

    made some distribution changes

--- lib/kymera/node.rb
+++ lib/kymera/node.rb
@@ -5,50 +5,52 @@ require_relative 'platform_utils'
 
 module Kymera
 
-  module Node
+  class Node
 
-    def self.start_server(redis_address, redis_port)
+    def start_server(redis_address, redis_port)
 
+      server_address = "tcp://#{Kymera.ip_address}:5521"
       begin
-        DCell.start :id => 'node_server', :addr => "tcp://#{Kymera.ip_address}:5521",
+        DCell.start :id => 'node_server', :addr => server_address ,
                     :registry => {
                         :adapter => 'redis',
                         :host    => redis_address,
                         :port    => redis_port.to_i
                     }
+          DCell::Global[:node_server] = server_address
       rescue
         raise "There was a problem starting the server.  The Redis instance was likely not started"
       end
     end
 
-    def self.register_node
+    def register_node
 
       DCell.start :id => "node_#{Kymera.host_name}", :addr => "tcp://#{Kymera.ip_address}:5521",
                   :directory => {
                        :id   => 'node_server',
-                       :addr => 'tcp://127.0.0.1:2042'
+                       :addr => DCell::Global[:node_server]
                    }
 
     end
 
-    def self.register_actors
+    def register_actors
       ActorGroup.run!
     end
-
-    def self.node_server_address
-      nodes = DCell::Node.all
-      address = nil
-      nodes.each do |node|
-
-        while address.nil? || i < 5
-          address = node.addr if node.id == 'node_server'
-
-        end
-
-      end
-
-
-    end
+    #
+    #def self.node_server_address
+    #  nodes = DCell::Node.all
+    #  address = nil
+    #  nodes.each do |node|
+    #
+    #    while address.nil? || i < 5
+    #      address = node.addr if node.id == 'node_server'
+    #
+    #    end
+    #
+    #  end
+    #
+    #
+    #end
 
 
   end
