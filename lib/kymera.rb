3ea485baf5c790901452a752a82ab246530d78ba
jakesa <jakes55214@yahoo.com>
2014-10-29 12:30:16 -0700

    a bit more clean up

--- lib/kymera.rb
+++ lib/kymera.rb
@@ -1,13 +1,16 @@
-#require "kymera/version"
-require_relative 'kymera/kymera'
+require 'ffi-rzmq'
+require_relative 'kymera/szmq/szmq'
+require_relative 'kymera/cucumber/cucumber_test_runner'
+require_relative 'kymera/worker'
+require_relative 'kymera/broker'
+require_relative 'kymera/client'
+require_relative 'kymera/cucumber/test_parser'
+require_relative 'kymera/results_bus'
 require_relative 'kymera/platform_utils'
-require_relative 'kymera/results_parser'
-require_relative 'kymera/test_parser'
-require_relative 'kymera/test_runner'
-require_relative 'kymera/actor'
-require_relative 'kymera/actors'
-require_relative 'kymera/node'
-require_relative 'kymera/config'
+require_relative 'kymera/test_results_collector'
+require_relative 'kymera/cucumber/cucumber_results_parser'
+
 module Kymera
-  # Your code goes here...
+
+
 end
