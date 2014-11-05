e19a67c8a039e89a47a46bb3f558b53790411385
jakesa <jakes55214@yahoo.com>
2014-11-04 16:03:35 -0800

    added a spec file to bring up all the core components of kymera

---
+++ lib/spec/startup_broker_bus_collector_spec.rb
@@ -0,0 +1,31 @@
+require 'kymera'
+threads = []
+
+trap('INT') do
+  threads.each do |thread|
+    thread.kill
+  end unless threads.empty?
+  @close = true
+end
+
+#Results bus thread
+threads << Thread.new {
+  bus = Kymera::ResultsBus.new('tcp://*:7000', 'tcp://*:7001')
+  bus.start_bus
+}
+
+#Broker thread
+threads << Thread.new {
+  broker = Kymera::Broker.new('tcp://*:5550', 'tcp://127.0.0.1:5551', 'tcp://*:5552', 20)
+  broker.start_broker
+}
+
+#Results collector thread
+threads << Thread.new {
+  results_collector = Kymera::TestResultsCollector.new('tcp://*:5556', 'tcp://127.0.0.1:7000')
+  results_collector.listen
+}
+
+loop do
+  raise SystemExit if @close
+end
